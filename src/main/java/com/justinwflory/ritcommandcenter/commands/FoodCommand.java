bd5e07ae589dabb32af801fa8e97b4cb6c02f5e1
Spencer Alderman <spencer@1rogue.com>
2015-11-04 15:03:33 -0500

    remove random args check

--- src/main/java/com/justinwflory/ritcommandcenter/commands/FoodCommand.java
+++ src/main/java/com/justinwflory/ritcommandcenter/commands/FoodCommand.java
@@ -17,58 +17,54 @@ public class FoodCommand implements SubCommand {
 
     @Override
     public void exec(CommandSender sender, String... args) {
-        if (args.length == 0) {
-            sender.sendMessage(ChatColor.DARK_RED + "Incorrect command syntax: try /rit food");
-        } else {
             try {
-                sender.sendMessage(ChatColor.GREEN + "Open places to eat: ");
-                Document doc = Jsoup.connect("https://rit-apps.modolabs.net/dining/index").get();
-                Elements open = doc.select("li.kgo_location_open");
-                for (Iterator localIterator1 = open.iterator(); localIterator1.hasNext(); ) {
-                    Element element = (Element) localIterator1.next();
-                    Elements ahrefList = element.getElementsByClass("kgoui_list_item_action");
-                    Element ahref = ahrefList.get(0);
-                    Elements adiv = ahref.getElementsByClass("kgoui_list_item_textblock");
-                    Element div = adiv.get(0);
-                    Elements subDiv = div.getAllElements();
-                    for (Iterator localIterator2 = subDiv.iterator(); localIterator2.hasNext(); ) {
-                        Element subDivInfo = (Element) localIterator2.next();
-                        sender.sendMessage(ChatColor.GOLD + subDivInfo.text());
-                    }
-                    sender.sendMessage("");
+            sender.sendMessage(ChatColor.GREEN + "Open places to eat: ");
+            Document doc = Jsoup.connect("https://rit-apps.modolabs.net/dining/index").get();
+            Elements open = doc.select("li.kgo_location_open");
+            for (Iterator localIterator1 = open.iterator(); localIterator1.hasNext(); ) {
+                Element element = (Element) localIterator1.next();
+                Elements ahrefList = element.getElementsByClass("kgoui_list_item_action");
+                Element ahref = ahrefList.get(0);
+                Elements adiv = ahref.getElementsByClass("kgoui_list_item_textblock");
+                Element div = adiv.get(0);
+                Elements subDiv = div.getAllElements();
+                for (Iterator localIterator2 = subDiv.iterator(); localIterator2.hasNext(); ) {
+                    Element subDivInfo = (Element) localIterator2.next();
+                    sender.sendMessage(ChatColor.GOLD + subDivInfo.text());
                 }
-                Element element;
-                Element subDivInfo;
-                sender.sendMessage(ChatColor.GREEN + "Closed places to eat: ");
-                Elements close = doc.select("li.kgo_location_closed");
-                for (Element element2 : close) {
-                    Elements ahrefList = element2.getElementsByClass("kgoui_list_item_action");
-                    Element ahref = (Element) ahrefList.get(0);
-                    Elements adiv = ahref.getElementsByClass("kgoui_list_item_textblock");
-                    Element div = adiv.get(0);
-                    Elements subDiv = div.getAllElements();
-                    for (Element subDivInfo2 : subDiv) {
-                        sender.sendMessage(ChatColor.RED + subDivInfo2.text());
-                    }
-                    sender.sendMessage("");
-                }
-                Document doc2 = Jsoup.connect("https://rit-apps.modolabs.net/dining/index?feed=dining&start=15").get();
-                Elements close2 = doc2.select("li.kgo_location_closed");
-                for (Element element2 : close2) {
-                    Elements ahrefList = element2.getElementsByClass("kgoui_list_item_action");
-                    Element ahref = ahrefList.get(0);
-                    Elements adiv = ahref.getElementsByClass("kgoui_list_item_textblock");
-                    Element div = adiv.get(0);
-                    Elements subDiv = div.getAllElements();
-                    for (Element subDivInfo2 : subDiv) {
-                        sender.sendMessage(ChatColor.RED + subDivInfo2.text());
-                    }
-                    sender.sendMessage("");
-                }
-            } catch (IOException e) {
-                Elements ahrefList;
-                e.printStackTrace();
+                sender.sendMessage("");
             }
+            Element element;
+            Element subDivInfo;
+            sender.sendMessage(ChatColor.GREEN + "Closed places to eat: ");
+            Elements close = doc.select("li.kgo_location_closed");
+            for (Element element2 : close) {
+                Elements ahrefList = element2.getElementsByClass("kgoui_list_item_action");
+                Element ahref = (Element) ahrefList.get(0);
+                Elements adiv = ahref.getElementsByClass("kgoui_list_item_textblock");
+                Element div = adiv.get(0);
+                Elements subDiv = div.getAllElements();
+                for (Element subDivInfo2 : subDiv) {
+                    sender.sendMessage(ChatColor.RED + subDivInfo2.text());
+                }
+                sender.sendMessage("");
+            }
+            Document doc2 = Jsoup.connect("https://rit-apps.modolabs.net/dining/index?feed=dining&start=15").get();
+            Elements close2 = doc2.select("li.kgo_location_closed");
+            for (Element element2 : close2) {
+                Elements ahrefList = element2.getElementsByClass("kgoui_list_item_action");
+                Element ahref = ahrefList.get(0);
+                Elements adiv = ahref.getElementsByClass("kgoui_list_item_textblock");
+                Element div = adiv.get(0);
+                Elements subDiv = div.getAllElements();
+                for (Element subDivInfo2 : subDiv) {
+                    sender.sendMessage(ChatColor.RED + subDivInfo2.text());
+                }
+                sender.sendMessage("");
+            }
+        } catch (IOException e) {
+            Elements ahrefList;
+            e.printStackTrace();
         }
     }
 
